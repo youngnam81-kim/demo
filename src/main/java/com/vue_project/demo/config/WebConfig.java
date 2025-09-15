268c8097de34d67d3b54e0a4adba26fc62f0ee85
youngnam81-kim <kimyn81@naver.com>
2025-09-15 17:31:42 +0900

    .

--- src/main/java/com/vue_project/demo/config/WebConfig.java
+++ src/main/java/com/vue_project/demo/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.vue_project.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     
+	@Value("${my.cors.allowed-origins}")
+    private String allowedOrigins;
+	
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Vue.js 서버 주소
+                .allowedOrigins(allowedOrigins.split(",")) // 콤마로 구분된 값들을 배열로 변환
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
